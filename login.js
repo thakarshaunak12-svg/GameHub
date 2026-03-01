4190f24f3c3052bc1df78f133b967d91502e78fc
Thakar Shaunak <thakarshaunak12@gmail.com>
2026-03-01 15:57:24 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,85 @@
+function login(){
+
+let username =
+document.getElementById("username").value;
+
+let password =
+document.getElementById("password").value;
+
+
+// SEND DATA TO RENDER SERVER
+
+fetch("https://gamehub-ey1e.onrender.com/login",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+username:username,
+password:password
+
+})
+
+})
+
+.then(response => response.json())
+
+.then(data => {
+
+if(data.success){
+
+// SAVE PLAYER NAME
+
+localStorage.setItem("player1",username);
+
+// GO TO HOME PAGE
+
+window.location.href="home.html";
+
+}
+
+else{
+
+alert("Wrong Username or Password");
+
+}
+
+})
+
+.catch(error => {
+
+alert("Server Error");
+
+console.log(error);
+
+});
+
+}
+
+
+
+function togglePassword(){
+
+let pass =
+document.getElementById("password");
+
+if(pass.type=="password"){
+pass.type="text";
+}
+else{
+pass.type="password";
+}
+
+}
+
+
+
+function goSignup(){
+
+window.location.href="signup.html";
+
+}
